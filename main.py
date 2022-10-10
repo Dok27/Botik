dc90c5592deb4f6803a3ecd28b957c371cef4820
Dok <dok.job@mail.ru>
2022-10-10 16:48:03 +1000

    add functions

--- main.py
+++ main.py
@@ -5,11 +5,13 @@ from config import TOKEN, APIkey
 import random
 import requests
 
-
 open_weather_token = APIkey
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+putin = 'Путин - красавчик'
+zelenskiy = 'зеленский - хуйлан'
 count = 0
+
 
 def main(city):
     get_weather(city, open_weather_token)
@@ -47,96 +49,72 @@ def get_weather(city, open_weather_token):
 async def echo_send(message: types.Message):
     Message = message.text.lower()
     message_array = Message.split()
+    randomizer = random.randint(1, 3)
+
     for text in message_array:
-        if "погод" in text:
+        def humans_win():
+            file = open('humans_wins.txt', 'a')
+            file.writelines('*' + '\n')
+            file = open('humans_wins.txt', 'r')
+            content = file.readlines()
+            if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
+                raz = 'раза'
+            else:
+                raz = 'раз'
+            file.close()
+            return f'Человеки выйграли {len(content)} {raz}'
+
+        def botik_win():
+            file = open('botik_wins.txt', 'a')
+            file.writelines('*' + '\n')
+            file = open('botik_wins.txt', 'r')
+            content = file.readlines()
+            if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
+                raz = 'раза'
+            else:
+                raz = 'раз'
+            file.close()
+            return f'Ботик выйграл {len(content)} {raz}'
+
+        if 'зеленск' in text:
+            await message.reply(zelenskiy)
+        elif 'путин' in text:
+            await message.reply(putin)
+        elif "погод" in text:
             await message.reply(get_weather("Хабаровск", open_weather_token))
             await message.reply(get_weather("Ростов-на-Дону", open_weather_token))
             await message.reply(get_weather("Кропоткин", open_weather_token))
-            
+
         elif 'камень' in text:
             if randomizer == 1:
                 await message.reply('Камень! \U0001f48e Ничья.')
             elif randomizer == 2:
-                await message.reply(f'Ножницы! \u2702\uFE0F Ты выйграл!')
-                file = open('humans_wins.txt', 'a')
-                file.writelines('*' + '\n')
-                file = open('humans_wins.txt', 'r')
-                content = file.readlines()
-                if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                    raz = 'раза'
-                else:
-                    raz = 'раз'
-                await message.answer(f'Человеки выйграли {len(content)} {raz}')
-                file.close()
+                await message.reply('Ножницы! \u2702\uFE0F Ты выйграл!')
+                await message.answer(humans_win())
             elif randomizer == 3:
-                await message.reply(f'Бумага!! \U0001f4c3 Ты проиграл! Аххахахах')
-                file = open('botik_wins.txt', 'a')
-                file.writelines('*' + '\n')
-                file = open('botik_wins.txt', 'r')
-                content = file.readlines()
-                if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                    raz = 'раза'
-                else:
-                    raz = 'раз'
-                await message.answer(f'Ботик выйграл {len(content)} {raz}')
-                file.close()
+                await message.reply('Хуй тебе!!! Бумага!! \U0001f4c3 Ты проиграл! Аххахахах')
+                await message.answer(botik_win())
 
         elif 'ножницы' in text:
             if randomizer == 1:
                 await message.reply('Ножницы! \u2702\uFE0F Ничья.')
             elif randomizer == 2:
-                await message.reply(f'Бумага! \U0001f4c3 Ты выйграл!')
-                file = open('humans_wins.txt', 'a')
-                file.writelines('*' + '\n')
-                file = open('humans_wins.txt', 'r')
-                content = file.readlines()
-                if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                    raz = 'раза'
-                else:
-                    raz = 'раз'
-                await message.answer(f'Человеки выйграли {len(content)} {raz}')
-                file.close()
+                await message.reply('Бумага! \U0001f4c3 Ты выйграл!')
+                await message.answer(humans_win())
             elif randomizer == 3:
-                await message.reply(f'Камень!! \U0001f48e Ты проиграл! Аххахахах')
-                file = open('botik_wins.txt', 'a')
-                file.writelines('*' + '\n')
-                file = open('botik_wins.txt', 'r')
-                content = file.readlines()
-                if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                    raz = 'раза'
-                else:
-                    raz = 'раз'
-                await message.answer(f'Ботик выйграл {len(content)} {raz}')
-                file.close()
+                await message.reply('Хуй тебе!!! Камень!! \U0001f48e Ты проиграл! Аххахахах')
+                await message.answer(botik_win())
 
         elif 'бумага' in text:
             if randomizer == 1:
                 await message.reply('Бумага! \U0001f4c3 Ничья.')
             elif randomizer == 2:
-                await message.reply(f'Камень! \U0001f48e Ты выйграл!')
-                file = open('humans_wins.txt', 'a')
-                file.writelines('*' + '\n')
-                file = open('humans_wins.txt', 'r')
-                content = file.readlines()
-                if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                    raz = 'раза'
-                else:
-                    raz = 'раз'
-                await message.answer(f'Человеки выйграли {len(content)} {raz}')
-                file.close()
+                await message.reply('Камень! \U0001f48e Ты выйграл!')
+                await message.answer(humans_win())
             elif randomizer == 3:
-                await message.reply(f'Ножницы!! \u2702\uFE0F Ты проиграл! Аххахахах')
-                file = open('botik_wins.txt', 'a')
-                file.writelines('*' + '\n')
-                file = open('botik_wins.txt', 'r')
-                content = file.readlines()
-                if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                    raz = 'раза'
-                else:
-                    raz = 'раз'
-                await message.answer(f'Ботик выйграл {len(content)} {raz}')
-                file.close()
+                await message.reply('Хуй тебе!!! Ножницы!! \u2702\uFE0F Ты проиграл! Аххахахах')
+                await message.answer(botik_win())
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
