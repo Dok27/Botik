a26e3193aa41d762e7ebda134bfeb200adff1e91
Dok27 <87178020+Dok27@users.noreply.github.com>
2022-12-05 20:54:01 +1000

    updated
    add comments

--- main.py
+++ main.py
@@ -1,22 +1,53 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from config import TOKEN, APIkey
+from config import TOKEN, APIkey, ChatID
 import random
 import requests
+import aioschedule
+import asyncio
 
 open_weather_token = APIkey
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-putin = 'Путин - красавчик'
-zelenskiy = 'зеленский - хуйлан'
 count = 0
+
+#ботик выдаёт слово дня на ангийском и его перевод
+async def word_print():
+    file = open('ENRUS.TXT', 'r', encoding='utf-8')
+    content = file.readlines()
+
+    def asd(n):
+        my_str1 = content[n].replace('\n', ' ').replace('\t', ' ')
+        my_str2 = content[n + 1].replace('\n', ' ').replace('\t', ' ')
+        my_str3 = content[n - 1].replace('\n', ' ').replace('\t', ' ')
+
+        if n % 2 == 0:
+            return f"Слово дня: {my_str1} - {my_str2}"
+        else:
+            return f"Слово дня: {my_str3} - {my_str1}"
+
+    file.close()
+
+    n = random.randint(1, (len(content) + 1))
+    await bot.send_message(chat_id=ChatID, text=asd(n))
+
+#таймер
+async def scheduler():
+    aioschedule.every().day.at("7:00").do(word_print)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+
+async def on_startup(_):
+    asyncio.create_task(scheduler())
 
 
 def main(city):
     get_weather(city, open_weather_token)
 
-
+#погода
 def get_weather(city, open_weather_token):
     code_to_smile = {
         "Clear": "Ясно \U00002600",
@@ -51,36 +82,32 @@ async def echo_send(message: types.Message):
     message_array = Message.split()
     randomizer = random.randint(1, 3)
 
+    def humans_win():
+        file = open('humans_wins.txt', 'a')
+        file.writelines('*' + '\n')
+        file = open('humans_wins.txt', 'r')
+        content = file.readlines()
+        if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
+            raz = 'раза'
+        else:
+            raz = 'раз'
+        file.close()
+        return f'Человеки выйграли {len(content)} {raz}'
+
+    def botik_win():
+        file = open('botik_wins.txt', 'a')
+        file.writelines('*' + '\n')
+        file = open('botik_wins.txt', 'r')
+        content = file.readlines()
+        if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
+            raz = 'раза'
+        else:
+            raz = 'раз'
+        file.close()
+        return f'Ботик выйграл {len(content)} {raz}'
+    
     for text in message_array:
-        def humans_win():
-            file = open('humans_wins.txt', 'a')
-            file.writelines('*' + '\n')
-            file = open('humans_wins.txt', 'r')
-            content = file.readlines()
-            if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                raz = 'раза'
-            else:
-                raz = 'раз'
-            file.close()
-            return f'Человеки выйграли {len(content)} {raz}'
-
-        def botik_win():
-            file = open('botik_wins.txt', 'a')
-            file.writelines('*' + '\n')
-            file = open('botik_wins.txt', 'r')
-            content = file.readlines()
-            if ((len(content) % 10) == 2) or ((len(content) % 10) == 3) or ((len(content) % 10) == 4):
-                raz = 'раза'
-            else:
-                raz = 'раз'
-            file.close()
-            return f'Ботик выйграл {len(content)} {raz}'
-
-        if 'зеленск' in text:
-            await message.reply(zelenskiy)
-        elif 'путин' in text:
-            await message.reply(putin)
-        elif "погод" in text:
+        if "погод" in text:
             await message.reply(get_weather("Хабаровск", open_weather_token))
             await message.reply(get_weather("Ростов-на-Дону", open_weather_token))
             await message.reply(get_weather("Кропоткин", open_weather_token))
@@ -92,7 +119,7 @@ async def echo_send(message: types.Message):
                 await message.reply('Ножницы! \u2702\uFE0F Ты выйграл!')
                 await message.answer(humans_win())
             elif randomizer == 3:
-                await message.reply('Хуй тебе!!! Бумага!! \U0001f4c3 Ты проиграл! Аххахахах')
+                await message.reply('Бумага!! \U0001f4c3 Ты проиграл! Аххахахах')
                 await message.answer(botik_win())
 
         elif 'ножницы' in text:
@@ -102,7 +129,7 @@ async def echo_send(message: types.Message):
                 await message.reply('Бумага! \U0001f4c3 Ты выйграл!')
                 await message.answer(humans_win())
             elif randomizer == 3:
-                await message.reply('Хуй тебе!!! Камень!! \U0001f48e Ты проиграл! Аххахахах')
+                await message.reply('Камень!! \U0001f48e Ты проиграл! Аххахахах')
                 await message.answer(botik_win())
 
         elif 'бумага' in text:
@@ -112,9 +139,9 @@ async def echo_send(message: types.Message):
                 await message.reply('Камень! \U0001f48e Ты выйграл!')
                 await message.answer(humans_win())
             elif randomizer == 3:
-                await message.reply('Хуй тебе!!! Ножницы!! \u2702\uFE0F Ты проиграл! Аххахахах')
+                await message.reply('Ножницы!! \u2702\uFE0F Ты проиграл! Аххахахах')
                 await message.answer(botik_win())
 
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
